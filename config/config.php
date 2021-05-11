7982cbfa31eff8f4d2e33a969e44ef86f05c0d5f
李万闽 <lwm@wunsun.com>
2021-05-11 11:37:07 +0800

    修改配置文件

--- config/config.php
+++ config/config.php
@@ -1,15 +1,11 @@
 <?php
 return [
-    'redis'         => 'default',
+    'redis'         => env('LBS_REDIS','default'),
 
     //集合名
     'geo_set_name'  => env('GEO_SET_NAME', 'LBS'),
 
     //搜寻定义的一些参数
-    'radium_option' => [
-        'WITHDIST' => true,
-        'SORT'     => 'asc',
-        'WITHHASH' => false,
-    ],
+    'radium_option' => ['WITHDIST'],
     'allow_unit'    => ['m', 'km', 'ft', 'mi']
 ];
